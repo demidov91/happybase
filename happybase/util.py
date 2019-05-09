1d9856eb1cd2e60c51d7cba99565f5f568001235
dzmitry <demidov91@gmail.com>
2019-05-10 00:21:45 +0300

    Add monkeypatched retryable logic.

--- happybase/util.py
+++ happybase/util.py
@@ -3,13 +3,15 @@ HappyBase utility module.
 
 These functions are not part of the public API.
 """
-
+import logging
 import re
 
 import six
+from functools import partial, wraps
 from six.moves import range
 
 CAPITALS = re.compile('([A-Z])')
+logger = logging.getLogger(__name__)
 
 
 try:
@@ -86,3 +88,109 @@ def bytes_increment(b):
             b[i] += 1
             return bytes(b[:i+1])
     return None
+
+
+def retryable(
+        func=None,
+        expected_exception=(),
+        *,
+        retry_count: int=1,
+        callback=None,
+):
+    if func is None:
+        return partial(
+            retryable,
+            expected_exception=expected_exception,
+            retry_count=retry_count,
+            callback=callback
+        )
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        i = 0
+
+        prev_exceptions = []
+
+        while True:
+            try:
+                return func(*args, **kwargs)
+            except expected_exception as e:
+                prev_exceptions.append(e)
+                if i >= retry_count:
+                    logger.error(
+                        'Got too many exceptions: %s\nRaising the last one.',
+                        prev_exceptions
+                    )
+                    raise e
+
+                logger.info(
+                    'Got expected exception %s while running %s. %s attempts left.',
+                    type(e).__name__,
+                    func.__name__ if hasattr(func, '__name__') else func,
+                    retry_count - i
+                )
+
+                if callback is not None:
+                    callback(*args, **kwargs)
+
+                i += 1
+
+    return wrapper
+
+
+def retryable_generator(
+        func=None,
+        expected_exception=(),
+        *,
+        retry_count: int=1,
+        callback=None,
+):
+    if func is None:
+        return partial(
+            retryable,
+            expected_exception=expected_exception,
+            retry_count=retry_count,
+            callback=callback
+        )
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        i = 0
+
+        prev_exceptions = []
+
+        while True:
+            try:
+                object_to_return = func(*args, **kwargs)
+
+                try:
+                    yield next(object_to_return)
+                except StopIteration:
+                    return
+                except GeneratorExit:
+                    pass
+            except expected_exception as e:
+                prev_exceptions.append(e)
+                if i >= retry_count:
+                    logger.error(
+                        'Got too many exceptions: %s\nRaising the last one.',
+                        prev_exceptions
+                    )
+                    raise e
+
+                logger.info(
+                    'Got expected exception %s while running %s. %s attempts left.',
+                    type(e).__name__,
+                    func.__name__ if hasattr(func, '__name__') else func,
+                    retry_count - i
+                )
+
+                if callback is not None:
+                    callback(*args, **kwargs)
+
+                i += 1
+            else:
+                yield from object_to_return
+                return
+
+    return wrapper
--- tests/test_util.py
+++ tests/test_util.py
@@ -3,8 +3,9 @@ HappyBase utility tests.
 """
 
 from codecs import decode, encode
+from unittest import mock
 
-from nose.tools import assert_equal, assert_less
+from nose.tools import assert_equal, assert_less, assert_raises
 
 import happybase.util as util
 
@@ -54,3 +55,68 @@ def test_bytes_increment():
 
     for s, expected in test_values:
         yield check, s, expected
+
+
+def test_retryable__once():
+    real_method = mock.Mock(side_effect=[ValueError, 42])
+    f = util.retryable(real_method, ValueError)
+
+    assert f(202, '03') == 42
+
+    assert real_method.call_count == 2
+    real_method.assert_called_with(202, '03')
+
+
+def test_retryable__once_with_callback():
+    callback_method = mock.Mock()
+    real_method = mock.Mock(side_effect=[ValueError, 42])
+    f = util.retryable(real_method, ValueError, callback=callback_method)
+
+    assert f(202, '03') == 42
+
+    assert real_method.call_count == 2
+    callback_method.assert_called_once_with(202, '03')
+    real_method.assert_called_with(202, '03')
+
+
+def test_retryable__twice_error():
+    real_method = mock.Mock(side_effect=[ValueError, ValueError, 42])
+    f = util.retryable(real_method, ValueError)
+
+    with assert_raises(ValueError):
+        f(202, '03')
+
+    assert real_method.call_count == 2
+    real_method.assert_called_with(202, '03')
+
+
+def test_retryable__once_but_different():
+    real_method = mock.Mock(side_effect=[NotImplementedError, 42])
+    f = util.retryable(real_method, ValueError)
+
+    with assert_raises(NotImplementedError):
+        f(202, '03')
+
+    real_method.assert_called_once_with(202, '03')
+
+
+def test_retryable__twice_error_still_working():
+    callback_method = mock.Mock()
+    real_method = mock.Mock(side_effect=[ValueError, ValueError, 42])
+    f = util.retryable(real_method, ValueError, retry_count=2, callback=callback_method)
+
+    assert f(202, '03') == 42
+
+    assert real_method.call_count == 3
+    assert callback_method.call_count == 2
+    real_method.assert_called_with(202, '03')
+
+
+def test_retryable__different_errors_still_working():
+    real_method = mock.Mock(side_effect=[NotImplementedError, ValueError, 42])
+    f = util.retryable(real_method, (ValueError, NotImplementedError), retry_count=2)
+
+    assert f(202, '03') == 42
+
+    assert real_method.call_count == 3
+    real_method.assert_called_with(202, '03')
